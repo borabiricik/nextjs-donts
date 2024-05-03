367045c68404b025e59b157e0e70456188301ee8
borabiricik <bora.bir496@gmail.com>
2024-05-03 19:23:32 +0300

    feat: use typed router example added to layout

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const UserDetailsPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">User Detail Page</h1>
+    </div>
+  )
+}
+
+export default UserDetailsPage
---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">All Users Page</h1>
+    </div>
+  )
+}
+
+export default UsersPage
